5bd83cc982731a055a936185611df3be272be75c
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2020-03-21 20:05:20 +0100

    Add simple d3 graph

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # projectbaselinerheinauersee.de
 Website of Project Baseline Rheinauer See
+
+## TODOs
+
+ - Add a legend to the measurement graph
+ - Add the visibility line to the graph
--- script.js
+++ script.js
@@ -1,6 +1,59 @@
 $(document).ready(function() {
+    // Light/Dark of navigation links
     $(".nav-link").on("click", function() {
         $(".nav-item").find(".active").removeClass("active");
         $(this).addClass("active");
+    });
+
+    // Graph with measurement values
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 10, bottom: 50, left: 50},
+        width = 1140 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    // define the line
+    var valueline = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.temperature); });
+
+    var svg = d3.select("#measurement")
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    d3.json("data.json", function(err, json) {
+        if (err) throw err;
+        
+        var data = json["data"];
+
+        data.forEach(element => {
+            var parsed = d3.timeParse("%d.%m.%Y");
+            element.date = parsed(element.date);
+        });
+        
+        // Scale the range of the data
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+        y.domain([0, d3.max(data, function(d) { return d.temperature; })]);
+
+        // Add the valueline path.
+        svg.append("path")
+            .data([data])
+            .attr("class", "line")
+            .attr("d", valueline);
+
+        // Add the X Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // Add the Y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
     });
 });
